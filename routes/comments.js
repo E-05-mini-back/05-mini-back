a9a49919e034071a95ce655ad33b8a5658445a80
azoong <shshinkitec2@gmail.com>
2022-08-15 12:12:49 +0900

    댓글 API 수정

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth_middlewares");
-const { Comments, Users } = require("../models")
+const { Comments, Users, Posts } = require("../models")
 
 // router.get("/", (req, res) => {
 //     ok : true,
@@ -18,13 +18,20 @@ router.post("/:postId", authMiddleware, async (req, res) => {
             ok: false,
             errorMessage: "로그인을 해주세요."
         })
-    } else if (!comment) {
+
+    }
+    else if( await Posts.findOne({where: {postId : postId}}) == null ){
+        res.status(400).json({
+            ok: false,
+            errorMessage: "해당 게시물을 찾을 수 없습니다.’."
+        })
+    }else if (!comment) {
         res.status(400).json({
             ok: false,
             errorMessage: "댓글 내용을 입력해주세요."
         })
     } else {
-        Comments.create({ comment, postId, userId });
+        await Comments.create({ comment, postId, userId });
         res.status(201).json({
             ok: true,
             message: "댓글을 생성하였습니다."
@@ -35,6 +42,12 @@ router.post("/:postId", authMiddleware, async (req, res) => {
 //댓글 조회
 router.get("/:postId", async (req, res) => {
     const { postId } = req.params
+    if( await Posts.findOne({where: {postId : postId}}) == null ){
+        res.status(400).json({
+            ok: false,
+            errorMessage: "해당 게시물을 찾을 수 없습니다.’."
+        })
+    }    
     const Comment = await Comments.findAll({
         where: { postId: postId },
         include: {
@@ -45,9 +58,10 @@ router.get("/:postId", async (req, res) => {
             ["createdAt", "DESC"],
         ],
     })
+    
     res.status(200).json({
         ok: true,
-        data: Comment.map((Comment) => ({
+        result: Comment.map((Comment) => ({
             comment: Comment.comment,
             loginId: Comment.loginId,
             date: Comment.createdAt
@@ -61,13 +75,24 @@ router.put("/:commentId", authMiddleware, async (req, res) => {
     const { commentId } = req.params
     const { comment } = req.body
     const Comment = await Comments.findOne({ where: { commentId: commentId } })
+    console.log(Comment)
 
     if (!userId) { //이미 미들웨어에서 걸러지기는함.
         res.status(400).json({
             ok: false,
             errorMessage: "로그인이 필요합니다."
         })
-    } else if (userId !== Comment.userId) {
+    }else if (!comment) {
+        res.status(400).json({
+            ok: false,
+            errorMessage: "댓글 내용을 입력해주세요."
+        })    
+    } else if(Comment == null){
+        res.status(400).json({
+            ok: false,
+            errorMessage: "존재하지 않는 댓글입니다."
+        })
+    }else if (userId !== Comment.userId) {
         res.status(400).json({
             ok: false,
             errorMessage: "본인 댓글만 수정 가능합니다."
@@ -91,6 +116,12 @@ router.delete("/:commentId", authMiddleware, async (req, res) => {
         res.status(400).json({
             ok : false,
             errorMessage: "로그인을 해주세요" })
+    } else if(Comment == null){
+            res.status(400).json({
+                ok: false,
+                errorMessage: "존재하지 않는 댓글입니다."
+            })            
+
     } else if (userId !== Comment.userId) {
         res.status(400).json({ 
             ok : false,
