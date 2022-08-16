b001aaa3ee9684ab8ff2fff5ce270a7e7f1abcc0
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-08-16 17:18:18 +0900

    토큰 인증 게시물 생성

--- swagger.js
+++ swagger.js
@@ -4,11 +4,32 @@ const swaggereJsdoc = require("swagger-jsdoc");
 const options = {
   swaggerDefinition: {
     components: {},
+    // openapi: "3.0.3",
     info: {
       title: "Test API",
       version: "1.0.0",
       description: "test api",
     },
+    // servers: [
+    //   {
+    //     url: "http://localhost:3000/api/",
+    //   },
+    // ],
+    // components: {
+    securityDefinitions: {
+      Authorization: {
+        type: "apiKey",
+        name: "authorization",
+        scheme: "bearer",
+        in: "header",
+      },
+    },
+    // },
+    security: [
+      {
+        Authorization: [],
+      },
+    ],
     host: "localhost:3000",
     basePath: "/api/",
   },
