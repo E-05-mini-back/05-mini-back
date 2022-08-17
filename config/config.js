5dcf629905a5d0a5d46c9479276333951db271f3
azoong <shshinkitec2@gmail.com>
2022-08-17 10:46:42 +0900

    config 환경변수 수정

--- config/config.js
+++ config/config.js
@@ -1,31 +1,31 @@
 
 require("dotenv").config()
-{
-    "development": {
-        "username": process.env.username,
-        "password": process.env.password,
-        "database": process.env.database,
-        "host": process.env.host",
-        "dialect": "mysql",
-        "logging": false,
-        "timezone": "+09:00",
-        "dialectOptions": {
-            "dateStrings": true,
-            "typeCast": true
+module.exports = {
+    development: {
+        username: process.env.username,
+        password: process.env.password,
+        database: process.env.database,
+        host: process.env.host,
+        dialect: "mysql",
+        logging: false,
+        timezone: "+09:00",
+        dialectOptions: {
+            dateStrings: true,
+            typeCast: true
         }
     },
-    "test": {
-        "username": "root",
-        "password": null,
-        "database": "database_test",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
+    test: {
+        username: "root",
+        password: null,
+        database: "database_test",
+        host: "127.0.0.1",
+        dialect: "mysql"
     },
-    "production": {
-        "username": "root",
-        "password": null,
-        "database": "database_production",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
+    production: {
+        username: "root",
+        password: null,
+        database: "database_production",
+        host: "127.0.0.1",
+        dialect: "mysql"
     }
 }
